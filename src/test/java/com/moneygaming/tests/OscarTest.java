38df793fe8bfab4d3a23fba98650bc3533f55eea
OscarSah <oscar@cybertekschool.com>
2021-09-18 19:36:39 +0300

    oscar branch commit

---
+++ src/test/java/com/moneygaming/tests/OscarTest.java
@@ -0,0 +1,13 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class OscarTest {
+
+    @Test
+    public void testOscar(){
+
+        System.out.println("my beautiful test");
+
+    }
+}
