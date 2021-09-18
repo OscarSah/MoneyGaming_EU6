6fb12dc963aec93c65e10d8a3424a6702c06247a
ratilgan <ekserjetik@gmail.com>
2021-09-18 19:39:35 +0300

    My first commit to branch

---
+++ src/test/java/com/moneygaming/tests/RamazanTest.java
@@ -0,0 +1,11 @@
+package com.moneygaming.tests;
+
+import org.testng.annotations.Test;
+
+public class RamazanTest {
+
+    @Test
+    public void testRamazan(){
+        System.out.println("my beautiful test");
+    }
+}
